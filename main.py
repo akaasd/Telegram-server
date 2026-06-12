1b49f58a1573d91db374feb8d6f9b6dc888d0b7d
akaasd <tuakapop@gmail.com>
2026-06-12 12:37:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             time_diff_ms = now_ms - stored_timestamp
             time_diff_minutes = time_diff_ms / (1000 * 60)
 
-            if time_diff_minutes <= 1380:  # 24 hours
+            if time_diff_minutes <= 1440:  # 24 hours
                 generate_new = False
 
         if generate_new or not stored_giftcode:
